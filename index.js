6fbced89793273662b8f5c9103e6e461d45ee831
今井 淳貴 <0504jun0504@gmail.com>
2016-08-27 16:12:25 +0900

    簡易四則計算まで終了。

--- index.js
+++ index.js
@@ -1,47 +1,96 @@
-var num1,num2,sum1;
+var num,num01,mark;
+num = 0;
+num01 = 0;
 
-function input(){
-  num1 = document.dentaku.num1.value;
-  num2 = document.dentaku.num2.value;
+//クリックで値を受け取る
+document.dentaku.num0.onclick = function num0(){
+  num01 = num01 + document.dentaku.num0.value;
+  console.log(num01);
+}
+document.dentaku.num1.onclick = function num1(){
+  num01 = num01 + document.dentaku.num1.value;
+  console.log(num01);
+}
+document.dentaku.num2.onclick = function num2(){
+  num01 = num01 + document.dentaku.num2.value;
+  console.log(num01);
+}
+document.dentaku.num3.onclick = function num3(){
+  num01 = num01 + document.dentaku.num3.value;
+  console.log(num01);
 }
 
-//足し算
-document.dentaku.sum1.onclick = function sum(){
-  input();
-  sum1 = parseInt(num1) + parseInt(num2);
-  console.log(sum1);
+
+function parseNum(){
+  num01 = parseInt(num01);
+  num = num01;
+  num01 = 0;
 }
 
-//引き算
-document.dentaku.sum2.onclick = function sum(){
-  input();
-  if (num1 > num2) {
-    sum1 = parseInt(num1) - parseInt(num2);
-    console.log(sum1);
+//四則計算
+document.dentaku.mark1.onclick = function mark1(){
+  parseNum();
+  mark = "+";
+  console.log(mark);
+  console.log(num);
+}
+document.dentaku.mark2.onclick = function mark2(){
+  parseNum();
+  mark = "-";
+  console.log(mark);
+  console.log(num);
+}
+document.dentaku.mark3.onclick = function mark3(){
+  parseNum();
+  mark = "*";
+  console.log(mark);
+  console.log(num);
+}
+document.dentaku.mark4.onclick = function mark4(){
+  parseNum();
+  mark = "/";
+  console.log(mark);
+  console.log(num);
+}
+
+//markに応じた処理を行う
+function calculation(){
+  if (mark == "+") {
+    num = num + num01;
+  } else if (mark == "-") {
+    num = num - num01;
+  } else if (mark == "*") {
+    num = num * num01;
+  } else if (mark == "/") {
+    num = num / num01;
   } else {
-    sum1 = 'エラーだ！！！';
-    console.log(sum1);
+    print('you used undifined mark...');
   }
 }
 
-//掛け算
-document.dentaku.sum3.onclick = function sum(){
-  input();
-  sum1 = parseInt(num1) * parseInt(num2);
-  console.log(sum1);
-}
-
-//割り算
-document.dentaku.sum4.onclick = function sum(){
-  input();
-  if (num1 > 0) {
-    sum1 = parseInt(num1) / parseInt(num2);
-    console.log(sum1);
-  } else if (num1 == 0 || num2 == 0) {
-    sum1 = 'エラーだ！！！';
-    console.log(sum1);
+//合計を計算する
+document.dentaku.sum.onclick = function sum(){
+  if (num01 == 0) {
+    num01 = num;
+    calculation();
+    num01 = 0;
+    console.log(num);
+  } else {
+    num = parseInt(num);
+    num01 = parseInt(num01);
+    calculation();
+    num01 = 0;
+    console.log(num);
   }
 }
+
+
+//変数の初期化
+document.dentaku.clear.onclick = function clear(){
+  num = 0;
+  num1 = 0;
+}
+
 
 
 
